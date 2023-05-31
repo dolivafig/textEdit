f1106810c8ceb07d0a071fb03fb0a8b9539275c3
gman <dolivafigueroa@gmail.com>
2023-05-30 20:57:56 -0400

    heroku commit

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -23,10 +24,7 @@ module.exports = () => {
         title: 'List'
       }),
 
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
+       new GenerateSW(),
 
       new WebpackPwaManifest({
         name: 'jate',
